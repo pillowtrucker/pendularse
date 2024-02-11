3bdd0a26fadfb3b070bf963483c56c07577d684f
pillowtrucker <githubino@janitor.chat>
2024-02-11 22:38:12 +0100

    start without the nonsense traits and macros

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+mod drpa;
+mod srpa;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(
+        short,
+        long,
+        default_value = "./inochi2d-models/arrows-doublespringpendulum.inp"
+    )]
+    puppet_path: String,
+    #[arg(short, long, default_value_t = 0)]
+    model_type: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+    match args.model_type {
+        0 => {
+            nannou::app(srpa::model).update(srpa::update).run();
+        }
+        1 => {
+            nannou::app(drpa::model).update(drpa::update).run();
+        }
+        _ => {}
+    };
+}
+/*
+struct Model {
+    _window: window::Id,
+}
+
+fn model(app: &App) -> Model {
+    let _window = app.new_window().view(view).event(window_event).build().unwrap();
+    Model { _window }
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+    draw.background().color(PLUM);
+    draw.ellipse().color(STEELBLUE);
+    draw.to_frame(app, &frame).unwrap();
+}
+fn window_event(app: &App, model: &mut Model, event: WindowEvent) {
+    match event {
+        KeyPressed(_key) => {}
+        KeyReleased(_key) => {}
+        ReceivedCharacter(_char) => {}
+        MouseMoved(_pos) => {}
+        MousePressed(_button) => {}
+        MouseReleased(_button) => {}
+        MouseEntered => {}
+        MouseExited => {}
+        MouseWheel(_amount, _phase) => {}
+        Moved(_pos) => {}
+        Resized(_size) => {}
+        Touch(_touch) => {}
+        TouchPressure(_pressure) => {}
+        HoveredFile(_path) => {}
+        DroppedFile(_path) => {}
+        HoveredFileCancelled => {}
+        Focused => {}
+        Unfocused => {}
+        Closed => {}
+    }
+}
+*/
